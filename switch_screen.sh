66b03185bfded401aa33135ed0036ce7b46d7474
zhangxingshuai <zhangxingshuai@harmonycloud.com.cn>
2019-07-19 14:32:34 +0800

    gengxin

---
+++ switch_screen.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ $(xrandr|grep HDMI1|cut -d ' ' -f 2) = 'connected' ];then
+	xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off;
+else 
+	xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off;
+fi
